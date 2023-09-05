73789038ceabdf0db9919b23b014587afb088f75
longhe.li <lilonghe@outlook.com>
2023-09-05 19:29:45 +0800

    fix: nav active check

--- management-system-react/src/components/layout/nav.tsx
+++ management-system-react/src/components/layout/nav.tsx
@@ -5,7 +5,7 @@ import { Link, matchPath } from 'react-router-dom'
 import routes, { IMyRoute } from '../../routes'
 import styles from './basicLayout.module.less'
 import { Key, ReactNode } from 'react'
-import type { MenuProps, MenuTheme } from 'antd'
+import type { MenuProps } from 'antd'
 
 type MenuItem = Required<MenuProps>['items'][number];
 function getItem (
@@ -25,18 +25,27 @@ function getItem (
 }
 
 export default function Nav () {
-  const rootActiveKey: string = ''
-  const isMatchNav = false
+  let rootActiveKey: string = ''
+  let isMatchNav = false
   let defaultActiveKey = location.pathname || '/'
 
   const getItems = (routeList: IMyRoute[], parent?: IMyRoute): MenuItem[] => {
     return routeList.map(route => {
+      const isMatchPath = matchPath(route.path, defaultActiveKey)
+
+      if (isMatchPath) {
+        isMatchNav = true
+      }
+
+      if (parent && isMatchPath) {
+        rootActiveKey = parent.path
+      }
       const label = route.isGroup ? route.title : <Link to={route.path}>{route.title}</Link>
       return getItem(label, route.path, route.icon, route.children && getItems(route.children, route))
     })
   }
 
-  const menuItems: MenuItem[] = getItems(routes.filter(route => route.isNav))
+  const menuItems: MenuItem[] = getItems(routes.filter(route => route.isNav && checkPermission(route.authority)))
 
   // 未匹配到绝对路由时，截取 url 最前缀当作上级导航
   // TODO: 但是如果是从耳机导航进入，应该高亮二级菜单

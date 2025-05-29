60c0602dcd88ec30ba952e0a38a0db6d1d72b535
longhe.li <lilonghe@outlook.com>
2025-05-29 14:12:45 +0800

    feat: add basic template

---
+++ basic/README.md
@@ -0,0 +1,7 @@
+# React basic template
+
+Vite + React + Typescript + TailwindCSS
+
+ESlint + Prettier
+
+Husky + lint-staged
---
+++ basic/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='text-3xl h-full flex justify-center items-center'>
+      Hello
+    </div>
+  );
+}
+
+export default App;
